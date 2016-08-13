265caae6facf3a1ddfaa3aab2618d47b30e83aa5
junjw1 <junjw1@daum.net>
2016-08-13 17:49:28 +0900

    ./server.js 수정..라우팅이 바뀌었다

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var Beer = require('./models/beer');
+var beerController = require('./controllers/beer');
 
 //beer-locker를 mongoDB에 연결하기
 mongoose.connect('mongodb://localhost:27017/beerlocker');
@@ -23,94 +23,15 @@ var port = process.env.PORT || 3000;
 //모든 express앱은 내장된(builtin) 앱 라우터를 가지고 있다. 라우터는 미들웨어처럼 행동 
 var router = express.Router();
 
-//라우트 생성. '/'을 위한 라우트를 만들고, 'json 객체(object)'를 리턴 
-router.get('/', function(req, res){
-   res.json({ message: 'you are running dangerously low on beer!!!~!~~!!~' }); 
-});
+router.route('/beers')
+.post(beerController.postBeers)
+.get(beerController.getBeers);
 
-//prefix가 /beers인 새 라우트 생성. 
-var beersRoute = router.route('/beers');
+router.route('/beers/:beer_id')
+.get(beerController.getBeer)
+.put(beerController.putBeer)
+.delete(beerController.deleteBeer);
 
-//POST를 위한 '/api/beers' 엔드포인트(endpoint) 생성.
-beersRoute.post(function(req, res){
-    /*우리가 엔드포인트에 POST할 때, 무엇을 할지 이 블록 안에 셋팅한다(setting up)*/
-
-
-    //Beer모델의 새 인스턴스 생성
-    var beer = new Beer();
-
-    //POST 데이터로부터 온 맥주 속성들(beer properties)을 셋
-    beer.name = req.body.name;
-    beer.type = req.body.type;
-    beer.quantity = req.body.quantity;
-
-    //맥주를 저장하고 에러를 체크
-    beer.save(function(err){
-        if(err){
-            res.send(err);
-        }
-        res.json({message: 'beer added to the locker!!', data: beer});
-    });
-
-
-});
-
-//GET을 위한 '/api/beers' 엔드포인트 생성
-beersRoute.get(function(req, res){
-    //모든 맥주를 찾기위해 맥주모델을 사용하자
-    Beer.find(function(err, beers){
-        if(err){
-            res.send(err);
-        }
-        res.json(beers);
-    });
-});
-
-// '/beers/:beer_id'가 prefix인 새 라우트 생성. 
-var beerRoute = router.route('/beers/:beer_id');
-
-// GET을 위한 엔드포인트 '/api/beers/:beer_id' 만들기, 
-beerRoute.get(function(req, res){
-    Beer.findById(req.params.beer_id, function(err, beer){
-        if(err){
-            res.send(err);
-        }
-        res.json(beer);
-    });
-});
-
-// PUT을 위한 엔드포인트 '/api/beers/:beer_id' 만들기
-beerRoute.put(function(req, res){
-    //특정 맥주를 찾기 위해 맥주모델 사용
-    Beer.findById(req.params.beer_id, function(err, beer){
-        if(err){
-            res.send(err);
-        }
-
-        //남은 맥주양 업데이트하기
-        beer.quantity = req.body.quantity;
-
-        //맥주 저장하고 에러 체크
-        beer.save(function(){
-            if(err){
-                res.send(err);
-            }
-            res.json(beer);
-        });
-    });
-});
-
-// DELETE를 위한 엔드포인트 '/api/beers/:beer_id' 생성
-beerRoute.delete(function(req, res){
-    //mongoose의 function인 findByIdAndRemove로 찾고 삭제하자.
-    Beer.findByIdAndRemove(req.params.beer_id, function(err){
-        if(err){
-            res.send(err);
-        }
-        res.json({message: 'beer removed from the locker!!'});
-
-    });
-});
 
 // '/api'에 모든 우리 라우트들을 등록하자(register 무엇을 with 여기에)
 // 우리가 이전에 정의했던 라우트(routes)를 앱에 지정하자. prefix '/api'를 사용해서.
