5cd8bdca7f4f9e49e8eac2198784e42dddf42ea8
junjw1 <junjw1@daum.net>
2016-08-13 23:15:51 +0900

    대소문자 주의! user 추가/보기 완료

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,9 @@
 //user와 추가된 user를 보기위한 controller
-var user = require('../models/user');
+var User = require('../models/user');
 
 //POST를 위한 엔드포인트 /api/users 생성
 exports.postUsers = function(req, res){
-    var user = new user({
+    var user = new User({
         username: req.body.username,
         password: req.body.password
     });
@@ -15,8 +15,8 @@ exports.postUsers = function(req, res){
 };
 
 //GET을 위한 엔드포인트 /api/users 생성
-epxorts.getUsers = function(req, res){
-    user.find(function(err, users){
+exports.getUsers = function(req, res){
+    User.find(function(err, users){
         if(err) res.send(err);
         res.json(users);
     });
